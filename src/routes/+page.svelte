2f541c665e5b25e47275a18b20ab7c032a565dea
Ryan Atkinson <mail@ryanatkn.com>
2022-11-21 13:23:06 -0600

    update title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Workspace from '@feltcoop/felt-server/ui/Workspace.svelte';
 
-	const title = 'felt-server';
+	const title = 'spiderspace.org';
 </script>
 
 <svelte:head><title>{title}</title></svelte:head>
