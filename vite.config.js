b4b29cb00dfbc7e80b26c31eabba64f050aa979e
Ryan Atkinson <mail@ryanatkn.com>
2022-11-21 09:04:44 -0600

    todo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import {sveltekit} from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	envPrefix: 'PUBLIC_',
-	optimizeDeps: {exclude: ['@feltcoop/felt']},
+	optimizeDeps: {exclude: ['@feltcoop/felt', '@feltcoop/felt-server']}, // TODO try to exclude after things work
 	server: {
 		proxy: {
 			'/api': 'http://localhost:3000', // same as in src/lib/config.ts
