b5fb35a7b9f1d3d0367d58c735dbfc26548981b5
Ryan Atkinson <mail@ryanatkn.com>
2022-11-20 23:23:04 -0600

    set proxy port

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	envPrefix: 'PUBLIC_',
 	optimizeDeps: {exclude: ['@feltcoop/felt']},
+	server: {
+		proxy: {
+			'/api': 'http://localhost:3000', // same as in src/lib/config.ts
+		},
+	},
 };
 
 export default config;
